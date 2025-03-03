a9a8a9395f50ef0928c123a13231d6b00d93d37c
kenfzhang <4808683+kenfzhang@users.noreply.github.com>
2025-03-02 20:38:52 -0800

    add bg image

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import tkinter as tk
 from tkinter import ttk
 
+from PIL import Image, ImageTk
+from tkinter import Label
+
 root = tk.Tk()
 root.title('Mindbuddy')
 root.geometry('600x400')
+
+# Load and convert the image
+img = Image.open("gh-pfp.png")
+img = ImageTk.PhotoImage(img)
+
+label1 = Label(root, image=img)
+label1.place(x=0, y=0)
+
+label1.image = img
 
 
 def button_clicked():
