f5c4a50f9c781755db4eb1b549be71b4ce8efcca
kenfzhang <4808683+kenfzhang@users.noreply.github.com>
2025-03-02 17:20:52 -0800

    window size, title, button effect

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import tkinter as tk
+from tkinter import ttk
 
 root = tk.Tk()
+root.title('Mindbuddy')
+root.geometry('600x400')
+
+
+def button_clicked():
+    print('Button clicked')
+    button.config(text="Wow!")
+
+
+button = ttk.Button(root, text='Click Me', command=button_clicked)
+button.pack()
+
 root.mainloop()
